04967470c72c06f11c60dd7db792764c9d61800e
Nirjas Jakilim <nirjas01@student.sust.edu>
2021-10-17 15:32:57 +0600

    Solved Sales By Match

--- Sales by Match.py
+++ Sales by Match.py
@@ -7,10 +7,10 @@
 # You have to determine how many pairs of socks with matching colors there are.
 
 # My Solution:
-# Each array reprsent the colors of the socks. As we have to find the matching pairs that means
-# we have to actually find the matching number pairs. For example: Suppose 10 is a color.
-# So, if there is another 10 in this array then we count it as a pair. So, we have to
-# actually count a occurance of a number and then divide it by 2 as we are only counting 2 as a pair.
+# Each array represents the colors of the socks.
+# As we have to find the matching pairs of socks that means we have to actually find the matching number of pairs.
+# For example: Suppose 10 is a color. So, if there is another 10 in this array then we will count it as a pair.
+# So, we have to actually count the occurrence of a number and then divide it by 2 as we are only counting 2 as a pair.
 
 # Importing Counter library
 from collections import Counter
@@ -24,7 +24,3 @@ if __name__== '__main__':
             c+=int(d[a]/2) # Dividing the occurances by 2.
             d[a] = int(d[a]%2) # Resetting the occurances back to 1 so that it can't be counted again.
     print(c)
-        
-    
-        
-    
