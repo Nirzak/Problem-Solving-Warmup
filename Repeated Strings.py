32115febbebf2090cc15862aaf57fdbcd435e6b6
Nirjas Jakilim <nirjas01@student.sust.edu>
2021-10-17 18:33:00 +0600

    Solved Repeated Strings

--- Repeated Strings.py
+++ Repeated Strings.py
@@ -7,13 +7,13 @@ Problem Statement:
 https://www.hackerrank.com/challenges/repeated-string/problem
 
 My Solution:
-The problem is mainly to count the number of a in a repeated string within a particular range. For example:
-Suppose a string is aba and n = 10 that means we have to find occurances of a in "abaabaabaa" this string.
-So, the solution is either building the string with a loop and then count the occurance of a in that string.
-But there is an optimal solution there. and this is just multiply the count of 'a' in the given string 'aba' with
-the value of 'n' and then divide it with the lenght of the string. It's the basic rule of Unitary method.
-But now there is a problem. It exclude the remainder string that is not full. In the above example which the extra
-'a' in the 10th position of the string "abaabaabaa". So we have count it separately and add with our previous answer.That's it.
+The problem is mainly to count the number of "a" in a repeated string within a particular range. For example:
+Suppose a string is "aba" and n = 10 which means we have to find occurrences of "a" in "abaabaabaa" this string.
+So, the solution is either building the string with a loop and then count the occurrence of "a" in that string.
+But there is an optimal solution there. and this is just multiplying the count of "a" of the given string "aba" with
+the value of "n" and then divide it with the length of the string. It's the basic rule of the Unitary method.
+But now there is a problem. It excludes the remaining string which is the extra "a" in the 10th position of the string "abaabaabaa".
+So we have to count it separately and add it with our previous answer. That's it.
 
 """
 st = input().strip()
